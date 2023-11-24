33a7c44f397fd4947ead021177601b8eee81558e
AravindR97 <aravindrajeshchry@gmail.com>
2023-11-24 22:04:54 +0530

    Added Package

---
+++ SentimentAnalysis/sentiment_analysis.py
@@ -0,0 +1,17 @@
+"""
+This module has a function 'sentiment_analyzer' which sends a request to Watson AI NLP library and returns
+the evaluated sentiment label and sentiment score to the App
+"""
+
+import requests
+
+def sentiment_analyzer(text_to_analyze):
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    obj = { "raw_document": { "text": text_to_analyze } }
+
+    r = requests.post(url, headers= header, json= obj)
+    result = r.json()
+    label = result['documentSentiment']['label']
+    score = result['documentSentiment']['score']
+    return {'label': label, 'score': score}
